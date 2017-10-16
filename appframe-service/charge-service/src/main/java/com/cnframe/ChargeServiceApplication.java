1eeca884d3fd75d3a8df184ce31d199aaad5ac0a
gelb <glbboy@163.com>
2017-10-16 18:55:27 +0800

    mysql连接配置更改

--- appframe-service/charge-service/src/main/java/com/cnframe/ChargeServiceApplication.java
+++ appframe-service/charge-service/src/main/java/com/cnframe/ChargeServiceApplication.java
@@ -2,8 +2,18 @@ package com.cnframe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cnframe.repository.BaseRepositoryFactoryBean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
+@RestController
+@RefreshScope
 public class ChargeServiceApplication {
 
 	public static void main(String[] args) {
